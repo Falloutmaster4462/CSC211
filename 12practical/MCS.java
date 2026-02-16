0869962acfdf279723b73a670537bfaf0f2d65f1
Jazeel <jazeelnilraj0@gmail.com>
2026-02-16 14:46:06 +0200

    Finished up the function mcsOn3

--- 12practical/MCS.java
+++ 12practical/MCS.java
@@ -21,7 +21,15 @@ public static int mcsOn3(int[] x){
 
     for (int low = 0; low < n; low++) {
         for (int high = low; high < n; high++) {
-
+             int sum =0;
+             for ( int r  = low; r <= high; r++ ) {
+                 sum += x[r];
+                 countOn3++;
+             }
+             if (sum > maxsofar) {
+                 maxsofar = sum;
+             }
         }
     }
+    return maxsofar;
 }
