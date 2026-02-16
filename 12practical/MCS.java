5e35cae40d7cab700060211714f3deb6501bd76e
Jazeel <jazeelnilraj0@gmail.com>
2026-02-16 14:41:39 +0200

    Working on the  Implementation for O(n^3):

--- 12practical/MCS.java
+++ 12practical/MCS.java
@@ -3,7 +3,7 @@
 //MCS Prac 16/02/2026
 
 
-// Importing claases
+// Importing clases
 import java.util.Random;
 
 //Counters and their initalisation
@@ -12,3 +12,16 @@ static long countOn2A = 0;
 static long countOn2B = 0;
 static long countOn = 0;
 
+
+// Implementation for O(n^3):
+
+public static int mcsOn3(int[] x){
+    int n = x.length;
+    int maxsofar = 0;
+
+    for (int low = 0; low < n; low++) {
+        for (int high = low; high < n; high++) {
+
+        }
+    }
+}
