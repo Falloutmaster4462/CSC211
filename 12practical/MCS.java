a89478722a3365b2e32765469905cf9a01deaea9
Jazeel <jazeelnilraj0@gmail.com>
2026-02-16 14:36:11 +0200

    Adding the counters and intialising them as well as the importing of the java classes

---
+++ 12practical/MCS.java
@@ -0,0 +1,14 @@
+//4551372
+//Nikaj Jazeel Nilraj
+//MCS Prac 16/02/2026
+
+
+// Importing claases
+import java.util.Random;
+
+//Counters and their initalisation
+static long countOn3 = 0;
+static long countOn2A = 0;
+static long countOn2B = 0;
+static long countOn = 0;
+
