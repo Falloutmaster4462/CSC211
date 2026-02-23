85242d9f923f54912eb9e27960df467af49145ee
Jazeel <jazeelnilraj0@gmail.com>
2026-02-23 14:43:26 +0200

    Created the base class for the program to start and created the node class.

--- 13Practical/src/Prac13_Main.java
+++ 13Practical/src/Prac13_Main.java
@@ -1,15 +1,21 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Prac13_Main {
-        public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.*;
+import java.util.*;
+import java.text.DecimalFormat;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Prac13_Main {
+
+    // Create the node class
+    static class Node {
+        //Initialse var
+        int key;
+        String data;
+
+        Node(int k, String d) {
+            key = k;
+            data = d;
+
         }
     }
+    S
+
 }
