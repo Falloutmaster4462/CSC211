35f171e0a0764713b6e480a772ce8acdd7afc775
Jazeel <jazeelnilraj0@gmail.com>
2026-03-02 14:57:37 +0200

    Created and finished the remove func

--- 14Practical/src/chainedHash.java
+++ 14Practical/src/chainedHash.java
@@ -1,11 +1,11 @@
 public class chainedHash {
 
-    private static  class Node{
+    private static class Node {
         String key;
         String value;
         Node next;
 
-        Node(String k, String v ){
+        Node(String k, String v) {
             key = key;
             value = value;
 
@@ -15,30 +15,30 @@ public class chainedHash {
     private Node[] table;
     private int m;
 
-    public chainedHash(int m){
+    public chainedHash(int m) {
         this.m = m;
         table = new Node[m + 1];
     }
 
-    private int hash(String key){
+    private int hash(String key) {
         int h = Math.abs(key.hashCode());
-        return (h%m) + 1;
+        return (h % m) + 1;
     }
 
-    public void insert( String key, String value){
+    public void insert(String key, String value) {
         int i = hash(key);
         Node head = table[i];
 
-        if (head == null){
+        if (head == null) {
             table[i] = new Node(key, value);
             return;
         }
         Node current = head;
         Node prev = null;
 
-        while (current != null){
+        while (current != null) {
 
-            if (current.key.equals(key)){
+            if (current.key.equals(key)) {
                 current.value = value;
                 return;
             }
@@ -47,17 +47,40 @@ public class chainedHash {
         }
         prev.next = new Node(key, value);
     }
-public String lookup(String key){
-    int i = hash(key);
-   Node current = table[i];
 
-    while (current != null){
-        if (current.key.equals(key)){
-            return current.value;
+    public String lookup(String key) {
+        int i = hash(key);
+        Node current = table[i];
+
+        while (current != null) {
+            if (current.key.equals(key)) {
+                return current.value;
+            }
+            current = current.next;
         }
-    current = current.next;
+        return null;
     }
-    return null;
-}
+
+    public String remove(String key) {
+        int i = hash(key);
+        Node current = table[i];
+        Node prev = null;
+        while (current != null) {
+            if (current.key.equals(key)) {
+                if (prev == null)
+                    table[i] = current.next;
+                else
+                    prev.next = current.next;
+                return current.value;
+            }
+            prev = current;
+            current = current.next;
+        }
+
+        return null;
+    }
 
 }
+
+
+
