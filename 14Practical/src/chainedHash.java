f197799041579912c284957a677035dadd6fb0af
Jazeel <jazeelnilraj0@gmail.com>
2026-03-02 14:45:59 +0200

    Created and finished the chainedhash func

--- 14Practical/src/chainedHash.java
+++ 14Practical/src/chainedHash.java
@@ -13,5 +13,23 @@ public class chainedHash {
     private Node[] table;
     private int m;
 
-    public chainedHash(int m){}
+    public chainedHash(int m){
+        this.m = m;
+        table = new Node[m + 1];
+    }
+
+    private int hash(String key){
+        int h = Math.abs(key.hashCode());
+        return (h%m) + 1;
+    }
+
+    public void insert( String key, String value){
+        int i = hash(key);
+        Node head = table[i];
+
+        if (head == null){
+            table[i] = new Node(key, value);
+            return;
+        }
+    }
 }
