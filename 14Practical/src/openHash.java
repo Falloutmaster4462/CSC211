90478b529da49823a1b4ca9f3fef8c9016b263b8
Jazeel <jazeelnilraj0@gmail.com>
2026-03-02 14:25:19 +0200

    Created the private node class

--- 14Practical/src/openHash.java
+++ 14Practical/src/openHash.java
@@ -1,4 +1,15 @@
 public class openHash {
 
 
+    private static  class Node{
+            String key;
+            String value;
+
+            Node(String k, String v ){
+                key = key;
+                value = value;
+            }
+    }
+
+
 }
