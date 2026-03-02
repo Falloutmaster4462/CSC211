b1b8b18a133850538a77cf1acf9065efc735fd81
Jazeel <jazeelnilraj0@gmail.com>
2026-03-02 14:10:32 +0200

    Created the Static variables as well the directory (14Practical) and the file Hash.java

---
+++ 14Practical/src/Hash.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Hash {
+
+    // Created static var
+    static final int N = 1 << 20; //2^20 = 1,048,576
+    static  final int MAX_USED = 950_000;
+    static  final int REPETITIONS = 30;
+
+    public static void main(String[] args) {
+
+    }
+}
