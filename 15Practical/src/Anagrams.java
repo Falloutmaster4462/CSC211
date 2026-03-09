34d39de17d1451879833596877a5e0084fe5860a
Jazeel <jazeelnilraj0@gmail.com>
2026-03-09 14:28:39 +0200

    Imported the joyce1922_ulysses.text and began to translate the Python into Java Code

---
+++ 15Practical/src/Anagrams.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class Anagrams {
+    public static void main(String[] args) {
+
+HashMap<String, ArrayList<String>> D = new HashMap<>();
+
+try{
+BufferedReader reader = new BufferedReader(new FileReader("joyce1922_ulysses.text"));
+String line;
+
+
+}
+
+    }
+}
