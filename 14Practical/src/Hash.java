a5f88269b13a3d57ebcf66c86b8e28fff4a1b339
Jazeel <jazeelnilraj0@gmail.com>
2026-03-09 14:17:26 +0200

    Created the 15 prac module

--- 14Practical/src/Hash.java
+++ 14Practical/src/Hash.java
@@ -3,7 +3,7 @@ public class Hash {
 
     // Created static var
     static final int N = 1 << 20; //2^20 = 1,048,576
-    static  final int MAX_USED = 950_000;
+    static  final int MAX_USED = 950000;
     static  final int REPETITIONS = 30;
 
     static class Pair{
