288b62a51d5063247a1864a790ccd4c4d83b2395
Jazeel <jazeelnilraj0@gmail.com>
2026-03-09 14:32:58 +0200

    Coninued working on the program and have "Cleaned" the words (No apostrophes) and have created the Signature

--- 15Practical/src/Anagrams.java
+++ 15Practical/src/Anagrams.java
@@ -9,7 +9,28 @@ try{
 BufferedReader reader = new BufferedReader(new FileReader("joyce1922_ulysses.text"));
 String line;
 
+while ((line = reader.readLine()) != null){
 
+    String[] words = line.split("\\s+");
+
+    for (String word : words){
+
+        //clean word (Leave apostrophes)
+        word = word.replaceAll("[...,;:_!\\-]+", "");
+        word = word.toLowerCase();
+
+        if (word.length() == 0){
+            continue;
+        }
+
+        // Create the Signature
+        char[] chars = word.toCharArray();
+        Arrays.sort(chars);
+        String key =  new String(chars);
+
+        //Insert into dictionary
+    }
+}
 }
 
     }
