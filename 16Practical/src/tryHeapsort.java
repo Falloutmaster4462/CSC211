6c8173cd187188d6d0e1b61115e4b694e784de87
Jazeel <jazeelnilraj0@gmail.com>
2026-03-16 14:07:59 +0200

    Starting the 16Practical activity

---
+++ 16Practical/src/tryHeapsort.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+public class tryHeapsort {
+
+        // Swap Utility
+            private static void swap(String[] arr, int i, int j) {
+            String temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        //Bottom-Up Heap Methods:
+
+     // Heapify node i in array of size n (min-heap for alphabetical order)
+}
