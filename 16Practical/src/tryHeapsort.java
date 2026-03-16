5e6b56bdfad52ea01906616849b85b0c39521add
Jazeel <jazeelnilraj0@gmail.com>
2026-03-16 14:12:25 +0200

    Finishd heapify void method

--- 16Practical/src/tryHeapsort.java
+++ 16Practical/src/tryHeapsort.java
@@ -11,4 +11,21 @@ public class tryHeapsort {
         //Bottom-Up Heap Methods:
 
      // Heapify node i in array of size n (min-heap for alphabetical order)
+
+    private static void heapify(String[] arr, int i, int j) {
+                int smallest = i;
+                int left = 2*i;
+                int right = 2*j + 1;
+
+
+                if (left < n && arr[left].compareTo(arr[smallest]) < 0)
+                    smallest = left;
+                if (right < n && arr[right].compareTo(arr[smallest]) < 0)
+                    smallest = right;
+
+                if (smallest != i) {
+                    swap(arr, i, smallest);
+                    heapify(arr, smallest, j);
+                }
+    }
 }
